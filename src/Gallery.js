29ce68dd3e1e8f722412332bc0f29481d34db8cd
Reiner Knudsen <reinerknudsen@me.com>
2023-11-14 21:17:41 +0100

    erste Dekomposition von Komponenten

---
+++ src/Gallery.js
@@ -0,0 +1,11 @@
+import Profile from './Profile';
+
+export default function Gallery() {
+  return(
+    <section>
+      <h1>My Profile Library</h1>
+      <Profile />
+      <Profile />
+    </section>
+  )
+};
